aa35fbe13d14188fb5aa6598d009670d234db546
roshanzamil <rshn.zamil@gmail.com>
2026-03-03 20:39:35 +0400

    added a

--- src/components/contact-us/index.tsx
+++ src/components/contact-us/index.tsx
@@ -4,16 +4,14 @@ import HeaderOne from "@/layouts/headers/HeaderOne";
 import Wrapper from "@/layouts/Wrapper";
 import ContactArea from "./ContactArea";
 
- 
-
-export default function Contactus() {
+export default function Contactus({ dictionary, lang }: { dictionary: any; lang: string }) {
   return (
     <Wrapper>
-      <HeaderOne />
+      <HeaderOne dictionary={dictionary} lang={lang} />
       <div id="smooth-wrapper">
         <div id="smooth-content" style={{ paddingTop: '120px' }}>
-          <ContactArea />
-          <FooterOne />
+          <ContactArea dictionary={dictionary.contact_us_page} lang={lang} />
+          <FooterOne dictionary={dictionary.footer} lang={lang} />
         </div>
       </div>
     </Wrapper>
