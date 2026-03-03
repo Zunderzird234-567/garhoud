b5ff56b924e50a16199a896a3823a2daabea1969
roshanzamil <rshn.zamil@gmail.com>
2026-03-03 20:30:58 +0400

    remove broken not_found route

--- src/app/[lang]/[...not_found]/page.tsx
+++
@@ -1,3 +0,0 @@
-// This file is part of an incorrect structure and is intentionally left blank
-// to prevent build errors. The correct 404 handling is implemented in
-// src/app/[lang]/not-found.tsx.
